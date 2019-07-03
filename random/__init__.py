d9c7a898e6a743055df6d7ede5b260da818b5bed
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-07-03 02:44:18 +0100

    initial commit

---
+++ random/__init__.py
@@ -0,0 +1,5 @@
+from .random import Random
+
+
+def setup(bot):
+    bot.add_cog(Random(bot))
---
+++ random/random.py
@@ -0,0 +1,32 @@
+import discord
+import typing
+from redbot.core import checks, commands
+
+
+class React(commands.Cog):
+    """
+    Simple reaction cog
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
+    async def reaction(
+        self, ctx: commands.Context, msg_id: int, emoji, channel: discord.TextChannel = None
+    ):
+        """React to a message.\n
+		Msg_id is the message ID you want to react to. \n
+        If a channel is not specified it will look for
+        the message ID in the current channel.
+        """
+        if not channel:
+            channel = ctx.channel
+        try:
+            msg = await channel.fetch_message(msg_id)
+            await msg.add_reaction(emoji)
+        except discord.HTTPException:
+            await ctx.send(
+                "That message ID isn't in this channel, please specify the channel the message is in or use the correct message ID."
+            )
