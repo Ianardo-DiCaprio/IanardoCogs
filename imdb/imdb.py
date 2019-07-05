fa40d285864f8686f61721ce039f27431ea1fd86
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-07-05 18:53:35 +0100

    Fuck you tabs

--- imdb/imdb.py
+++ imdb/imdb.py
@@ -7,30 +7,30 @@ class IMDB(commands.Cog):
         self.bot = bot
         self.conf = Config.get_conf(self, identifier=6991142013)
 		
-		default_global = (
-		"api_key" = None,
-		)
+        default_global = (
+            "api_key" = None,
+        )
 		
-		self.config.register_global(**default_global)
+        self.config.register_global(**default_global)
 		
 		
     @commands.command()
-	@checks.is_owner()
+    @checks.is_owner()
     async def imdbapi(self, ctx, api_key):
         """Command to set the IMDB API key"""
         await self.config.api_key.set(api_key)
-		await ctx.send("The API key has been set.")
+        await ctx.send("The API key has been set.")
 		
 		
     @commands.command()
     async def imdb(self, ctx, search):
         """Command to get information from IMDB"""
         api_key = await self.config.api_key()
-		search = search.replace(" ", "+")
+        search = search.replace(" ", "+")
         response = unirest.get("https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s={search}",
         headers={
         "X-RapidAPI-Host": "movie-database-imdb-alternative.p.rapidapi.com",
         }
         ).format(search=search)
-		await ctx.send(response)
+        await ctx.send(response)
 		
