9e1d9d616f8da138c302052d17a3bc1359479fca
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-07-05 20:59:32 +0100

    Update imdb.py

--- imdb/imdb.py
+++ imdb/imdb.py
@@ -26,21 +26,7 @@ class IMDB(commands.Cog):
         search = search.replace(" ", "+")
         r = requests.get(("http://www.omdbapi.com/?apikey={api_key}&s={search}").format(api_key=api_key, search=search))
         data = r.json()
-        title = data["Title"]
-        age_rating = data["Rated"]
-        release_date = data["Released"]
-        genre = data["Genre"]
-        director = data["Director"]
-        actors = data["Actors"]
-        plot = data["Plot"]
-        imdb_rating = data["imdbRating"]
+        title = data["Search"][0]["Title"]
         embed=discord.Embed(title=title)
         embed.set_thumbnail(url="https://m.media-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_SX300.jpg")
-        embed.add_field(name="Age rating", value=age_rating, inline=False)
-        embed.add_field(name="Release Date", value=release_date, inline=False)
-        embed.add_field(name="Genre", value=genre, inline=False)
-        embed.add_field(name="Director", value=director, inline=False)
-        embed.add_field(name="Actors", value=actors, inline=False)
-        embed.add_field(name="Plot", value=plot, inline=False)
-        embed.add_field(name="IMDB Rating", value=imdb_rating, inline=False)
         await ctx.send(embed=embed)
