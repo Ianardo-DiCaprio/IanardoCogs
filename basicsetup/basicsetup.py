334ae182d729285b98ca38c29dc449ecc65aa6ca
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-07-18 21:23:58 +0100

    Invoke instead of send.
    Now invokes the commands instead of sending a message with what commands to use.

--- basicsetup/basicsetup.py
+++ basicsetup/basicsetup.py
@@ -1,8 +1,10 @@
 import asyncio
+import discord
 
 from redbot.core import commands, checks
 from redbot.core.utils.chat_formatting import box
 from redbot.core.utils.predicates import MessagePredicate
+from discord.ext.commands import TextChannelConverter
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -27,6 +29,8 @@ class BasicSetup(BaseCog):
         predicate = MessagePredicate.yes_or_no(ctx, ctx.channel, ctx.author)
         predicate1 = MessagePredicate.greater(0, ctx, ctx.channel, ctx.author)
         predicate2 = MessagePredicate.length_less(200, ctx, ctx.channel, ctx.author)
+        predicate3 = MessagePredicate.length_less(200, ctx, ctx.channel, discord.Role)
+        
 
         q1 = "Would you like to setup Anti Mention Spam?"
         q2 = "Would you like the user to be banned when exceeding the max mentions?"
@@ -57,16 +61,17 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q1, predicate) == 'yes':
                     number = await self._get_response(ctx, "How many mentions from a single user in a single message should be acted upon?", predicate1)
-                    await ctx.send("`]antimentionspam max {}`".format(number))
+                    number = int(number)
+                    await ctx.invoke(ctx.bot.get_command("antimentionspam max"), number)
                     await asyncio.sleep(1)
                     await self._get_response(ctx, q2, predicate) == 'yes'
-                    await ctx.send("`]antimantionspam autobantoggle`")
+                    await ctx.invoke(ctx.bot.get_command("antimentionspam autobantoggle"))
                     await asyncio.sleep(1)
                     await self._get_response(ctx, q3, predicate) == 'yes'
-                    number = await self._get_response(ctx, "How many mentions?", predicate1)
+                    number = int(await self._get_response(ctx, "How many mentions?", predicate1))
                     await asyncio.sleep(0.5)
-                    number1 = await self._get_response(ctx, "In how many seconds?", predicate1)
-                    await ctx.send("`]antimentionspam maxinterval {} {}`".format(number, number1))
+                    number1 = int(await self._get_response(ctx, "In how many seconds?", predicate1))
+                    await ctx.invoke(ctx.bot.get_command("antimentionspam  maxinterval"), number, number1)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -74,10 +79,11 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("Autorole")
             if cog:
                 if await self._get_response(ctx, q4, predicate) == 'yes':
-                    await ctx.send("`]autorole toggle`")
+                    await ctx.invoke(ctx.bot.get_command("autorole toggle"))
                     await asyncio.sleep(1)
-                    role = await self._get_response(ctx, "What role would you like to be added to new users?", predicate2)
-                    await ctx.send("`]autorole add {}`".format(role))
+                    rolemessage = await self._get_response(ctx, "What role would you like to be added to new users?", predicate2)
+                    role = await commands.RoleConverter().convert(ctx, rolemessage)
+                    await ctx.invoke(ctx.bot.get_command("autorole add"), role=role)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -85,8 +91,9 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("BanCheck")
             if cog:
                 if await self._get_response(ctx, q5, predicate) == 'yes':
-                    channel = await self._get_response(ctx, "What channel would you like to be used as log?", predicate2)
-                    await ctx.send("`]bancheck channel {}`".format(channel))
+                    channelmessage = await self._get_response(ctx, "What channel would you like to be used as log?", predicate2)
+                    channel = await (TextChannelConverter()).convert(ctx, channelmessage)
+                    await ctx.invoke(ctx.bot.get_command("bancheckset enablechannel"), channel)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -95,10 +102,10 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q6, predicate) == 'yes':
                     name = await self._get_response(ctx, "What would you like the bank to be called?", predicate2)
-                    await ctx.send("`]bankset bankname {}`".format(name))
+                    await ctx.invoke(ctx.bot.get_command("bankset bankname"), name=name)
                     await asyncio.sleep(1)
                     name1 = await self._get_response(ctx, "What would you like the credits to be called?", predicate2)
-                    await ctx.send("`]bankset creditsname {}`".format(name1))
+                    await ctx.invoke(ctx.bot.get_command("bankset creditsname"), name=name1)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -106,7 +113,7 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("Grenzpolizei")
             if cog:
                 if await self._get_response(ctx, q7, predicate) == 'yes':
-                    await ctx.send("`]gp autosetup`")
+                    await ctx.invoke(ctx.bot.get_command("gp autosetup"))
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -115,7 +122,7 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q8, predicate) == 'yes':
                     name = await self._get_response(ctx, "What would you like the casino to be called?", predicate2)
-                    await ctx.send("`]casinoset name {}`".format(name))
+                    await ctx.invoke(ctx.bot.get_command("casinoset name"), name=name)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -123,7 +130,7 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("CleverBot")
             if cog:
                 if await self._get_response(ctx, q9, predicate) == 'yes':
-                    await ctx.send("`]cleverbotset toggle`")
+                    await ctx.invoke(ctx.bot.get_command("cleverbotset toggle"))
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -131,16 +138,16 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("Dungeon")
             if cog:
                 if await self._get_response(ctx, q10, predicate) == 'yes':
-                    await ctx.send("`]dungeon toggle`")
+                    await ctx.invoke(ctx.bot.get_command("dungeon toggle"))
                     await asyncio.sleep(0.5)
-                    await ctx.send("`]dungeon autoban`")
+                    await ctx.invoke(ctx.bot.get_command("dungeon autoban"))
                     await asyncio.sleep(1)
                     days = await self._get_response(ctx, "How many days old must an account be before I automatically ban them?", predicate1)
-                    await ctx.send("`]dungeon joindays {}`".format(days))
+                    await ctx.invoke(ctx.bot.get_command("dungeon joindays"), days=days)
                     await asyncio.sleep(1)
                     ans = await self._get_response(ctx, "Would you like users with default profile pics to be banned?", predicate)
                     if ans == 'yes':
-                        await ctx.send("`]dungeon profiletoggle`")
+                        await ctx.invoke(ctx.bot.get_command("dungeon profiletoggle`"))
                         await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -149,19 +156,19 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q11, predicate) == 'yes':
                     amount = await self._get_response(ctx, "How many credits should be given for using the payday command?", predicate1)
-                    await ctx.send("`]economyset paydayamount {}`".format(amount))
+                    await ctx.invoke(ctx.bot.get_command("economyset paydayamount"), creds=amount)
                     await asyncio.sleep(1)
                     ans = await self._get_response(ctx, "Would you like to change the cooldown time for the payday command?", predicate)
                     if ans == 'yes':
                         await asyncio.sleep(1)
-                        time = await self._get_response(ctx, "How many seconds would you like the cooldown to be?", predicate1)
-                        await ctx.send("`]economyset paydaytime {}`".format(time))
+                        time = int(await self._get_response(ctx, "How many seconds would you like the cooldown to be?", predicate1))
+                        await ctx.invoke(ctx.bot.get_command("economyset paydaytime"), seconds=time)
                         await asyncio.sleep(1)
                         register = await self._get_response(ctx, "Would you like to change the amount of credits new accounts start with?", predicate)
                         if register == 'yes':
                             await asyncio.sleep(1)
                             amount1 = await self._get_response(ctx, "How many credits would you like new accounts to start with?", predicate1)
-                            await ctx.send("`]economyset registeramount {}`".format(amount1))
+                            await ctx.invoke(ctx.bot.get_command("economyset registeramount"), creds=amount1)
                             await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -170,7 +177,7 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q12, predicate) == 'yes':
                     filt = await self._get_response(ctx, "What words would you like to be added to the filter? (seperate each word with a space, if it is a sentence or phrase put it insidel ""quotes"")", predicate2)
-                    await ctx.send("`]filter add {}`".format(filt))
+                    await ctx.invoke(ctx.bot.get_command("filter add"), words=filt)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -178,7 +185,7 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("InfoChannel")
             if cog:
                 if await self._get_response(ctx, q13, predicate) == 'yes':
-                    await ctx.send("`]infochannel`")
+                    await ctx.invoke(ctx.bot.get_command("infochannel"))
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -186,7 +193,7 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("Leaver")
             if cog:
                 if await self._get_response(ctx, q14, predicate) == 'yes':
-                    await ctx.send("Use `]leaverset channel` in the channel you wish to recieve the messages")
+                    await ctx.send("**Use `]leaverset channel` in the channel you wish to recieve the messages**")
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -195,8 +202,9 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q15, predicate) == 'yes':
                     await asyncio.sleep(1)
-                    channel = await self._get_response(ctx, "What channel would you like the lyrics to go to?", predicate2)
-                    await ctx.send("`]lyricset channel {}`".format(channel))
+                    channelmessage = await self._get_response(ctx, "What channel would you like the lyrics to go to?", predicate2)
+                    channel = await (TextChannelConverter()).convert(ctx, channelmessage)
+                    await ctx.invoke(ctx.bot.get_command("lyricset channel"), channel_name=channel)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -207,13 +215,13 @@ class BasicSetup(BaseCog):
                     repeats = await self._get_response(ctx, "Would you like to automatically delete repeated messages?", predicate)
                     if repeats == 'yes':
                         await asyncio.sleep(1)
-                        await ctx.send("`]modset deleterepeats`")
+                        await ctx.invoke(ctx.bot.get_command("modset deleterepeats"))
                         await asyncio.sleep(1)
                         delay = await self._get_response(ctx, "Would you like to automatically delete command messages?", predicate)
                         if delay == 'yes':
                             await asyncio.sleep(1)
                             delay1 = await self._get_response(ctx, "How many seconds should I wait before deleting them?", predicate1)
-                            await ctx.send("`]modset deletedelay {}`".format(delay1))
+                            await ctx.invoke(ctx.bot.get_command("modset deletedelay"), time=delay1)
                             await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -222,8 +230,9 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q17, predicate) == 'yes':
                     await asyncio.sleep(1)
-                    channel = await self._get_response(ctx, "What channel would you like the logs to go to?", predicate2)
-                    await ctx.send("`]modlogset modlog {}`".format(channel))
+                    channelmessage = await self._get_response(ctx, "What channel would you like the logs to go to?", predicate2)
+                    channel = await (TextChannelConverter()).convert(ctx, channelmessage)
+                    await ctx.invoke(ctx.bot.get_command("modlogset modlog"), channel=channel)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -232,10 +241,11 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q18, predicate) == 'yes':
                     await asyncio.sleep(1)
-                    channel = await self._get_response(ctx, "What channel would you like the reports to go to?", predicate2)
-                    await ctx.send("`]reportset toggle`")
+                    channelmessage = await self._get_response(ctx, "What channel would you like the reports to go to?", predicate2)
+                    channel = await (TextChannelConverter()).convert(ctx, channelmessage)
+                    await ctx.invoke(ctx.bot.get_command("reportset toggle"))
                     await asyncio.sleep(0.5)
-                    await ctx.send("`]reportset output {}`".format(channel))
+                    await ctx.invoke(ctx.bot.get_command("reportset output"), channel=channel)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -246,8 +256,9 @@ class BasicSetup(BaseCog):
                     await asyncio.sleep(1)
                     name = await self._get_response(ctx, "What would you like the starboard to be called?", predicate2)
                     await asyncio.sleep(0.5)
-                    channel = await self._get_response(ctx, "What channel would you like the starboard to be in?", predicate2)
-                    await ctx.send("`]starboard create {} {}`".format(name, channel))
+                    channelmessage = await self._get_response(ctx, "What channel would you like the starboard to be in?", predicate2)
+                    channel = await (TextChannelConverter()).convert(ctx, channelmessage)
+                    await ctx.invoke(ctx.bot.get_command("starboard create"), name=name, channel=channel)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -255,7 +266,7 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("Tickets")
             if cog:
                 if await self._get_response(ctx, q20, predicate) == 'yes':
-                    await ctx.send("`]ticket set setup`")
+                    await ctx.invoke(ctx.bot.get_command("ticket set setup"))
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -263,12 +274,12 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("Welcome")
             if cog:
                 if await self._get_response(ctx, q21, predicate) == 'yes':
-                    await ctx.send("`]welcomeset leave toggle`\n"
-                    "`]welcomeset ban toggle`\n"
-                    "`]welcomeset unban toggle`\n"
-                    "`]welcomeset toggle`\n"
-                    "`]welcomeset join msg add`\n"
-                    "**After you have added the new message do the following command and then type the number of the old default message**\n"
+                    await ctx.invoke(ctx.bot.get_command("welcomeset leave toggle"))
+                    await ctx.invoke(ctx.bot.get_command("welcomeset ban toggle"))
+                    await ctx.invoke(ctx.bot.get_command("welcomeset unban toggle"))
+                    await ctx.invoke(ctx.bot.get_command("welcomeset toggle"))
+                    await ctx.send("Now do [p]welcomeset join msg add")
+                    await ctx.send("**After you have added the new message do the following command and then type the number of the old default message**\n"
                     "`]welcomeset join msg del`")
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
@@ -278,11 +289,12 @@ class BasicSetup(BaseCog):
             if cog:
                 if await self._get_response(ctx, q22, predicate) == 'yes':
                     await asyncio.sleep(1)
-                    role = await self._get_response(ctx, "What role would you like mods to have?", predicate2)
-                    await ctx.send("`]set modrole {}`".format(role))
+                    rolemessage = await self._get_response(ctx, "What role would you like mods to have?", predicate2)
+                    role = await commands.RoleConverter().convert(ctx, rolemessage)
+                    await ctx.invoke(ctx.bot.get_command("set addmodrole"), role=role)
                     await asyncio.sleep(1)
                     role1 = await self._get_response(ctx, "What role would you like admins to have?", predicate2)
-                    await ctx.send("`]set adminrole {}`".format(role1))
+                    await ctx.invoke(ctx.bot.get_command("set addadminrole"), role=role1)
                     await asyncio.sleep(1)
         except asyncio.TimeoutError:
             pass
@@ -290,8 +302,8 @@ class BasicSetup(BaseCog):
             cog = self.bot.get_cog("ReactRoles")
             if cog:
                 if await self._get_response(ctx, q23, predicate) == 'yes':
-                    await ctx.send("`]reactroles add`\n"
-                    "**Hint: To get the message ID, turn on Developer mode in Discord's appearance settings and then right click the message and click copy ID **")
+                    await ctx.send("Use `]reactroles add ` with the message ID, channel, emoji and role.\n"
+                    "**Hint: To get the message ID, turn on developer mode in Discord's appearance settings and then right click the message and click copy ID **")
         except asyncio.TimeoutError:
             return
 
