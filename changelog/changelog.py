6c41a5a0c6144d1b99f47f693b1b7673df79d48f
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-05 00:12:00 +0100

    pylint + black

--- changelog/changelog.py
+++ changelog/changelog.py
@@ -1,5 +1,3 @@
-import io
-import contextlib
 import os
 
 import discord
@@ -8,9 +6,8 @@ from redbot.core import checks
 from redbot.core import commands
 from redbot.core.data_manager import cog_data_path
 from .announcer import Announcer
-
-from .utils import parse_time
 from .yaml_parse import embed_from_userstr
+
 
 class ChangeLog(commands.Cog):
     """
@@ -19,22 +16,17 @@ class ChangeLog(commands.Cog):
 
     __version__ = "1.0.0"
 
-
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(
-            self, identifier=3468763865834, force_registration=True
-        )
-        default_guild = {
-            "announce_ignore": True,
-            "announce_channel": None,
-        }
-        
+        self.config = Config.get_conf(self, identifier=3468763865834, force_registration=True)
+        default_guild = {"announce_ignore": True, "announce_channel": None}
+
         self.__current_announcer = None
 
         self.config.register_guild(**default_guild)
 
     def cog_unload(self):
+        """Cog Unload Docstring"""
         try:
             self.__current_announcer.cancel()
         except AttributeError:
@@ -42,6 +34,7 @@ class ChangeLog(commands.Cog):
 
     @staticmethod
     async def complain(ctx: commands.Context, message: str, **kwargs):
+        """"Docstring"""
         await ctx.send(message.format(**kwargs))
 
     def is_announcing(self) -> bool:
@@ -53,17 +46,13 @@ class ChangeLog(commands.Cog):
             return False
 
         return self.__current_announcer.active or False
-        
-    def check(m):
-        return m.id == ctx.message.id or m.author == victim
 
     @commands.group(name="log", autohelp=True)
     async def _log(self, ctx: commands.Context):
         """
         Changelog commands
         """
-        pass
-        
+
     @_log.command(name="upload")
     @commands.bot_has_permissions(embed_links=True)
     @checks.is_owner()
@@ -74,50 +63,48 @@ class ChangeLog(commands.Cog):
         called changelog.yml.
         """
         try:
-            with io.BytesIO() as fp:
-                dir = cog_data_path(self) / "changelog0.yml"
-                a = cog_data_path(self) / "changelog1.yml"
-                b = cog_data_path(self) / "changelog2.yml"
-                if os.path.exists(a):
-                    os.rename(a, b)
-                if os.path.exists(dir):
-                    os.rename(dir, a)
-                await ctx.message.attachments[0].save(dir)
-                await ctx.send("Changelog updated")
-                await self._autolog_send(ctx)
-        except Exception:
+            direct = cog_data_path(self) / "changelog0.yml"
+            change = cog_data_path(self) / "changelog1.yml"
+            change1 = cog_data_path(self) / "changelog2.yml"
+            if os.path.exists(change):
+                os.rename(change, change1)
+            if os.path.exists(direct):
+                os.rename(direct, change)
+            await ctx.message.attachments[0].save(direct)
+            await ctx.send("Changelog updated")
+            await self._autolog_send(ctx)
+        except discord.HTTPException:
             await ctx.send("That didn't work.", delete_after=30)
 
     @_log.command(name="get", pass_context=True)
     @commands.bot_has_permissions(embed_links=True)
-    async def get(self, ctx, number : str = 0):
+    async def get(self, ctx, number: str = 0):
         """
         Get changelogs for the bot
         """
         try:
-            dir = cog_data_path(self) / "changelog{}.yml".format(number)
+            direct = cog_data_path(self) / "changelog{}.yml".format(number)
             await ctx.message.delete()
-            fp = open(dir, "r")
-            data = fp.read()
-            e = await embed_from_userstr(ctx, data)
-            await ctx.send(embed=e)
-        except Exception:
+            info = open(direct, "r")
+            data = info.read()
+            embed = await embed_from_userstr(ctx, data)
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
             await ctx.send("That changelog doesn't exist.", delete_after=30)
-    
+
     @commands.group(name="autolog", autohelp=True)
     async def _autolog(self, ctx: commands.Context):
         """
         Autolog commands
         """
-        pass
-        
+
     async def _autolog_send(self, ctx: commands.Context):
         """Send out automatic changelogs"""
-        
-        dir = cog_data_path(self) / "changelog0.yml"
+
+        direct = cog_data_path(self) / "changelog0.yml"
         await ctx.message.delete()
-        fp = open(dir, "r")
-        data = fp.read()
+        info = open(direct, "r")
+        data = info.read()
         message = await embed_from_userstr(ctx, data)
         if not self.is_announcing():
             announcer = Announcer(ctx, message, config=self.config)
@@ -127,12 +114,13 @@ class ChangeLog(commands.Cog):
 
             await ctx.send("The changelogs have started to send.")
         else:
-            RUNNING_ANNOUNCEMENT = (
-            "I am already sending changelogs. If you would like to cancel"
-            " please use `{prefix}autolog cancel`"
-            " first.")
+            announcement = (
+                "I am already sending changelogs. If you would like to cancel"
+                " please use `{prefix}autolog cancel`"
+                " first."
+            )
             prefix = ctx.prefix
-            await self.complain(ctx, RUNNING_ANNOUNCEMENT, prefix=prefix)
+            await self.complain(ctx, announcement, prefix=prefix)
             await ctx.send("Changelogs sent.")
 
     @_autolog.command(name="cancel")
@@ -169,7 +157,9 @@ class ChangeLog(commands.Cog):
 
         if ignored:
             await ctx.send(
-                ("The server {guild.name} will receive automatic changelogs .").format(guild=ctx.guild)
+                ("The server {guild.name} will receive automatic changelogs .").format(
+                    guild=ctx.guild
+                )
             )
         else:
             await ctx.send(
