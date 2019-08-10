24ef260961c2e8b379286d22a7adc95dfcba79b5
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-10 17:42:38 +0100

    Update gamertag.py

--- gamertag/gamertag.py
+++ gamertag/gamertag.py
@@ -10,41 +10,48 @@ class GamerTag(commands.Cog):
         self.bot = bot
         self.conf = Config.get_conf(self, identifier=699114201327)
 
-        default_user = {"gamertag": None}
+        default_user = {
+            "playlstationgamertag": None,
+            "xboxgamertag": None,
+            "epicgamesgamertag": None,
+            "battlenetgamertag": None,
+            "uplaygamertag": None,
+            "steamgamertag": None,
+            }
 
         self.conf.register_user(**default_user)
 
     @commands.command()
-    async def gtset(self, ctx, gamertag=None):
-        """Command to set yout gamertag"""
+    async def psset(self, ctx, gamertag=None):
+        """Command to set your playstation gamertag"""
         if gamertag:
-            await self.conf.user(ctx.author).gamertag.set(gamertag)
-            await ctx.send("Your gamertag has been set.")
+            await self.conf.user(ctx.author).playstationgamertag.set(gamertag)
+            await ctx.send("Your playstation gamertag has been set.")
         else:
-            await self.conf.user(ctx.author).gamertag.set(gamertag)
-            await ctx.send("Your gamertag has been removed.")
+            await self.conf.user(ctx.author).playsationgamertag.set(gamertag)
+            await ctx.send("Your playstation gamertag has been removed.")
 
-    @commands.command(aliases=["gt"])
-    async def gamertag(self, ctx, user: discord.Member = None):
-        """Command to get a users gamertag if no user is given it will get yours."""
+    @commands.command(aliases=["psgt"])
+    async def psgamertag(self, ctx, user: discord.Member = None):
+        """Command to get a users Playstation gamertag if no user is given it will get yours."""
         if user is None:
             user = ctx.author
-        gamertag = await self.conf.user(user).gamertag()
-        if gamertag:
-            await ctx.send(f"This user's gamertag is: {gamertag}")
+        psgamertag = await self.conf.user(user).playsationgamertag()
+        if psgamertag:
+            await ctx.send(f"This user's Playstation gamertag is: {psgamertag}")
         else:
-            await ctx.send("This user hasn't set a gamertag.")
+            await ctx.send("This user hasn't set a Playstation gamertag.")
 
     @commands.command()
-    async def gtlist(self, ctx):
-        """Command to get a users gamertag if no user is given it will get yours."""
+    async def psgtlist(self, ctx):
+        """Command to get a list of users Playstation gamertags"""
         embeds = []
         msg = ""
         users = await self.conf.all_users()
-        for user_id, gamertag in users.items():
-            gamertagitems = gamertag.items()
+        for user_id, playstationgamertag in users.items():
+            gamertagitems = playstationgamertag.items()
             for gamer, tag in gamertagitems:
-                msg += f"<@{user_id}>'s gamertag is: {tag}\n"
+                msg += f"<@{user_id}>'s Playstation gamertag is: {tag}\n"
         embed = discord.Embed(title="Gamertags", description=msg, color=0x8C05D2)
         embeds.append(embed)
         await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=20)
