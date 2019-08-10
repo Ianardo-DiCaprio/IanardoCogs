7422b952473e404e70d4966f656a647804c52f31
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-10 15:54:02 +0100

    Update gamertag.py

--- gamertag/gamertag.py
+++ gamertag/gamertag.py
@@ -40,6 +40,6 @@ class GamerTag(commands.Cog):
         """Command to get a users gamertag if no user is given it will get yours."""
         users = await self.conf.all_users()
         for user_id, gamertag in users.items():
-            await ctx.send(f"<@{user_id}> gamertag is {gamertag.keys}")
+            await ctx.send(f"<@{user_id}> gamertag is {gamertag.items}")
             
 		
