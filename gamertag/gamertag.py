d2a639f273e63482d31a3cb9d91deccc22b0d0eb
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-10 16:11:00 +0100

    Update gamertag.py

--- gamertag/gamertag.py
+++ gamertag/gamertag.py
@@ -1,6 +1,6 @@
 import discord
 from redbot.core import commands, checks, Config
-from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
+from ..utils.chat_formatting import box, pagify
 
 class GamerTag(commands.Cog):
 
@@ -39,16 +39,14 @@ class GamerTag(commands.Cog):
     @commands.command()
     async def gtlist(self, ctx):
         """Command to get a users gamertag if no user is given it will get yours."""
-        embeds = []
+        pages = []
         users = await self.conf.all_users()
         for user_id, gamertag in users.items():
             gamertagitems = gamertag.items()
             for k, v in gamertagitems:
-                msg = list(f"<@{user_id}>'s gamertag is: {v}")
+                msg = (f"<@{user_id}>'s gamertag is: {v}")
                 embed = discord.Embed(title="Gamertags", description=msg, color=0x8C05D2)
-                embeds.append(embed)
-                await menu(
-                    ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=20
-                )
+                pages.append(embed)
+                await self.send_pages(ctx, pages, embed=True)
             
 		
