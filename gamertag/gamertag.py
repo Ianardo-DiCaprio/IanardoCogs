8d63856ac93d72e2b4a3faec02a54d0e5d4916c0
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-10 17:07:22 +0100

    black

--- gamertag/gamertag.py
+++ gamertag/gamertag.py
@@ -1,31 +1,31 @@
 import discord
-from redbot.core import commands, checks, Config
+from redbot.core import commands, Config
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
+
 class GamerTag(commands.Cog):
+    """GamerTag cog"""
 
     def __init__(self, bot):
         self.bot = bot
         self.conf = Config.get_conf(self, identifier=699114201327)
-		
+
         default_user = {"gamertag": None}
-		
+
         self.conf.register_user(**default_user)
-		
-		
+
     @commands.command()
-    async def gtset(self, ctx, gamertag = None):
+    async def gtset(self, ctx, gamertag=None):
         """Command to set yout gamertag"""
         if gamertag:
             await self.conf.user(ctx.author).gamertag.set(gamertag)
             await ctx.send("Your gamertag has been set.")
         else:
             await self.conf.user(ctx.author).gamertag.set(gamertag)
-            await ctx.send("Your gamertag has been removed.") 
-		
-		
-    @commands.command()
-    async def gt(self, ctx, user: discord.Member = None):
+            await ctx.send("Your gamertag has been removed.")
+
+    @commands.command(aliases=["gt"])
+    async def gamertag(self, ctx, user: discord.Member = None):
         """Command to get a users gamertag if no user is given it will get yours."""
         if user is None:
             user = ctx.author
@@ -35,7 +35,6 @@ class GamerTag(commands.Cog):
         else:
             await ctx.send("This user hasn't set a gamertag.")
 
-
     @commands.command()
     async def gtlist(self, ctx):
         """Command to get a users gamertag if no user is given it will get yours."""
@@ -44,12 +43,8 @@ class GamerTag(commands.Cog):
         users = await self.conf.all_users()
         for user_id, gamertag in users.items():
             gamertagitems = gamertag.items()
-            for k, v in gamertagitems:
-                msg += f"<@{user_id}>'s gamertag is: {v}\n"
+            for tag in gamertagitems:
+                msg += f"<@{user_id}>'s gamertag is: {tag}\n"
         embed = discord.Embed(title="Gamertags", description=msg, color=0x8C05D2)
         embeds.append(embed)
-        await menu(
-            ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=20
-        )
-            
-		
+        await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=20)
