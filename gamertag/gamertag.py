6a550428a87b6f1a6fec745d3a9663209f090603
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-10 18:23:45 +0100

    Update gamertag.py

--- gamertag/gamertag.py
+++ gamertag/gamertag.py
@@ -77,7 +77,7 @@ class GamerTag(commands.Cog):
             for k, v in gamertagitems:
                 if "xboxgamertag" in k:
                     for x in v:
-                        msg += f"<@{user_id}>'s Xbox gamertag is: {(x)}\n"
+                        msg += f"<@{user_id}>'s Xbox gamertag is: {x}\n"
         embed = discord.Embed(title="Gamertags", description=msg, color=0x8C05D2)
         embeds.append(embed)
         await menu(ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=20)
