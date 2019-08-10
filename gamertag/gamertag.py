5f05f434523e1467fac063c634fb32d312df9ac9
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-10 18:00:53 +0100

    Update gamertag.py

--- gamertag/gamertag.py
+++ gamertag/gamertag.py
@@ -73,7 +73,7 @@ class GamerTag(commands.Cog):
         msg = ""
         users = await self.conf.all_users()
         for user_id, xboxgamertag in users.items():
-            for gamer, tag in users.items("playstationgamertag")
+            for gamer, tag in users.items("playstationgamertag"):
                 msg += f"<@{user_id}>'s Playstation gamertag is: {tag}\n"
         embed = discord.Embed(title="Gamertags", description=msg, color=0x8C05D2)
         embeds.append(embed)
