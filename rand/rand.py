6057c1ae1edddf83095e154558750749acf5ea4e
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-08-16 06:08:37 +0100

    Update rand.py

--- rand/rand.py
+++ rand/rand.py
@@ -37,8 +37,10 @@ class Rand(commands.Cog):
     @commands.command(name="s")
     @checks.is_owner()
     async def _say(self, ctx, *, text: str = ""):
-    if text != "":
-        msg = await ctx.send(text)
-        await ctx.message.delete()
-    else:
-        await ctx.send("Please include some text for me to say")
+        """Make the bot say something and delete the command
+        that was used to make the bot say something."""
+        if text != "":
+            msg = await ctx.send(text)
+            await ctx.message.delete()
+        else:
+            await ctx.send("Please include some text for me to say")
