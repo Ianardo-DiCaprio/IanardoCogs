35a67e8e442cda703f6371218b828396caf66cbc
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-12-01 05:29:33 +0000

    Update codstats.py

--- codstats/codstats.py
+++ codstats/codstats.py
@@ -26,34 +26,31 @@ class CODSTATS(commands.Cog):
                 f"https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/{platform}/gamer/{username}/profile/type/mp"
         ) as request:
             data = await request.json()
-        try:
-            username = data["username"]
-            level = data["level"]
-            usernamelvl = username + level
-            embed = discord.Embed(title=usernamelvl, color=0x8C05D2)
-            if data["kills"] != "N/A":
-                embed.add_field(name="Kills", value=data["kills"], inline=True)
-            if data["kdRatio"] != "N/A":
-                embed.add_field(name="K/D Ratio", value=data["kdRatio"], inline=True)
-            if data["recordKillStreak"] != "N/A":
-                embed.add_field(name="Highest Killstreak", value=data["recordKillStreak"], inline=True)
-            if data["accuracy"] != "N/A":
-                embed.add_field(name="Average Accuracy", value=data["accuracy"], inline=True)
-            if data["wins"] != "N/A":
-                embed.add_field(name="Total Wins", value=data["wins"], inline=True)
-            if data["winLossRatio"] != "N/A":
-                embed.add_field(name="Win/Loss Ratio", value=data["winLossRatio"], inline=True)
-            if data["currentWinStreak"] != "N/A":
-                embed.add_field(name="Current Win Streak", value=data["currentWinStreak"], inline=True)
-            if data["gamesPlayed"] != "N/A":
-                embed.add_field(name="Games Played", value=data["gamesPlayed"], inline=True)
-            if data["scorePerMinute"] != "N/A":
-                embed.add_field(name="Score Per Minute", value=data["scorePerMinute"], inline=True)
-            if data["timePlayedTotal"] != "N/A":
-                embed.add_field(name="Total Play Time (in minutes)", value=data["timePlayedTotal"], inline=True)
-            embeds.append(embed)
-            await menu(
-                ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=20
-            )
-        except KeyError:
-            await ctx.send("I done fucked up :worried:")
+        username = data["username"]
+        level = data["level"]
+        usernamelvl = username + level
+        embed = discord.Embed(title=usernamelvl, color=0x8C05D2)
+        if data["kills"] != "N/A":
+            embed.add_field(name="Kills", value=data["kills"], inline=True)
+        if data["kdRatio"] != "N/A":
+            embed.add_field(name="K/D Ratio", value=data["kdRatio"], inline=True)
+        if data["recordKillStreak"] != "N/A":
+            embed.add_field(name="Highest Killstreak", value=data["recordKillStreak"], inline=True)
+        if data["accuracy"] != "N/A":
+            embed.add_field(name="Average Accuracy", value=data["accuracy"], inline=True)
+        if data["wins"] != "N/A":
+            embed.add_field(name="Total Wins", value=data["wins"], inline=True)
+        if data["winLossRatio"] != "N/A":
+            embed.add_field(name="Win/Loss Ratio", value=data["winLossRatio"], inline=True)
+        if data["currentWinStreak"] != "N/A":
+            embed.add_field(name="Current Win Streak", value=data["currentWinStreak"], inline=True)
+        if data["gamesPlayed"] != "N/A":
+            embed.add_field(name="Games Played", value=data["gamesPlayed"], inline=True)
+        if data["scorePerMinute"] != "N/A":
+            embed.add_field(name="Score Per Minute", value=data["scorePerMinute"], inline=True)
+        if data["timePlayedTotal"] != "N/A":
+            embed.add_field(name="Total Play Time (in minutes)", value=data["timePlayedTotal"], inline=True)
+        embeds.append(embed)
+        await menu(
+            ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=20
+        )
