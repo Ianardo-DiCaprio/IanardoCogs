2e24f8dd13d090ea28d58c28fad286304f326d2a
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-12-06 08:51:58 +0000

    Update directm.py

--- directm/directm.py
+++ directm/directm.py
@@ -7,14 +7,9 @@ class DirectM(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def allow_in_dm(self, ctx):
-        """Checks if the bank is global and allows the command in dm"""
-        if ctx.guild is None:
-            return True
-
     @commands.Cog.listener()
     async def on_message_without_command(self, ctx):
-        if await self.allow_in_dm(ctx):
+        if ctx.guild is None:
             channel = ctx.channel
             return await channel.send("This command is not available in DM's on this bot.")
 			
