0261e0c38b31162cf6b1b57ed331087563f63c83
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-12-06 08:50:26 +0000

    Update directm.py

--- directm/directm.py
+++ directm/directm.py
@@ -15,5 +15,6 @@ class DirectM(commands.Cog):
     @commands.Cog.listener()
     async def on_message_without_command(self, ctx):
         if await self.allow_in_dm(ctx):
-            return await ctx.send("This command is not available in DM's on this bot.")
+            channel = ctx.channel
+            return await channel.send("This command is not available in DM's on this bot.")
 			
