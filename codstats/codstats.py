c8c9590516d87be019ff30dd2dca91a0208783ae
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-12-14 22:42:22 +0000

    Add files via upload

--- codstats/codstats.py
+++ codstats/codstats.py
@@ -396,6 +396,19 @@ class CODSTATS(commands.Cog):
                 headshots = round(data["data"]["lifetime"]["itemData"]["weapon_dmr"]["iw8_sn_mike14"]["properties"]["headShots"])
                 embed.add_field(name="**EBR-14 Stats**", value=f"**Kills:** {ebr} \n **Headshots:** {headshots}", inline=True)
             embeds.append(embed)
+            #Melee Stats
+            embed = discord.Embed(title=userlvl + " - Melee Stats", color=0x8C05D2)
+            embed.set_thumbnail(url="https://i.pinimg.com/originals/cb/ea/43/cbea438a032192c7aa8210e596e4c065.png")
+            embed.set_footer(text="React to change pages for more stats!")
+            if data["data"]["lifetime"]["itemData"]["weapon_other"]["iw8_me_riotshield"]["properties"]["hits"] != "N/A":
+                riothit = round(data["data"]["lifetime"]["itemData"]["weapon_other"]["iw8_me_riotshield"]["properties"]["hits"])
+                riotkill = round(data["data"]["lifetime"]["itemData"]["weapon_other"]["iw8_me_riotshield"]["properties"]["kills"])
+                riotob = round(data["data"]["accoladeData"]["properties"]["riotShieldDamageAbsorbed"])
+                embed.add_field(name="**Riotshieled Stats**", value=f"**Hits:** {riothit} \n **Kills:** {riotkill} \n **Damage Obsorbed:** {riotob}", inline=True)
+            if data["data"]["lifetime"]["itemData"]["weapon_melee"]["iw8_knife"]["properties"]["hits"] != "N/A":
+                knifekill = round(data["data"]["lifetime"]["itemData"]["weapon_melee"]["iw8_knife"]["properties"]["hits"])
+                embed.add_field(name="**Combat Knife Stats**", value=f"**Kills:** {knifekill}", inline=True)
+            embeds.append(embed)
             #Domination Stats
             embed = discord.Embed(title=userlvl + " - Domination Stats", color=0x8C05D2)
             embed.set_thumbnail(url="https://i.pinimg.com/originals/cb/ea/43/cbea438a032192c7aa8210e596e4c065.png")
