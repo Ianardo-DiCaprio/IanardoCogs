9a90e1867710d8faa4cb84dc3ef127088158fed8
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2019-12-22 20:57:45 +0000

    Add files via upload

--- lookingfg/lookingfg.py
+++ lookingfg/lookingfg.py
@@ -31,57 +31,64 @@ class LookingFG(Cog):
         """Make an LFG request."""
         author = ctx.author
         bot = self.bot
+        expected = ("⏮", "⏹", "⏯", "⏭")
+        emoji = {"prev": "⏮", "stop": "⏹", "pause": "⏯", "next": "⏭"}
         try:
             await author.send(
                 "You have a maximum of 2 minutes to answer each question, "
-                "**Please specify what gamemode you are playing. eg: 3's**"
+                "Please specify what gamemode you are playing. eg: 3's"
             )
         except discord.Forbidden:
-            return await ctx.send("**I can't seem to be able to DM you. Do you have DM's closed?**")
+            return await ctx.send("I can't seem to be able to DM you. Do you have DM's closed?")
 
-        message = await ctx.send("**Okay, {0}, I've sent you a DM.**".format(author.mention))
+        messsage = await ctx.send("Okay, {0}, I've sent you a DM.".format(author.mention))
 
         def check(member):
             return member.author == author and member.channel == author.dm_channel
 
         try:
-            gamemode = await bot.wait_for("message", timeout=120, check=check)
+            gamemode = await bot.wait_for("reaction_add", timeout=120, check=ReactionPredicate.with_emojis(expected, message, ctx.author))
+            reacts = {v: k for k, v in emoji.items()}
+            react = reacts[r.emoji]
+            if react == "prev":
+                await self._clear_react(message, emoji)
+                gamemode = "1's"
         except asyncio.TimeoutError:
             return await ctx.send("You took too long. Try again.")
 
-        await author.send("**What rank are you in this gamemode? e.g: Champion**")
+        await author.send("What rank are you in this gamemode? e.g: Champion")
         try:
             rank = await bot.wait_for("message", timeout=120, check=check)
         except asyncio.TimeoutError:
             return await ctx.send("You took too long. Try again.")
 
-        await author.send("**What servers are you playing on? e.g: EU, USE**")
+        await author.send("What servers are you playing on? e.g: EU, USE")
         try:
             server = await bot.wait_for("message", timeout=120, check=check)
         except asyncio.TimeoutError:
             return await ctx.send("You took too long. Try again.")
 
-        await author.send("**What platform are you using? e.g: PC**")
+        await author.send("What platform are you using? e.g: PC")
         try:
             platform = await bot.wait_for("message", timeout=120, check=check)
         except asyncio.TimeoutError:
             return await ctx.send("You took too long. Try again.")
 
-        await author.send("**How many people are you looking for?**")
+        await author.send("How many people are you looking for?")
         try:
             amount = await bot.wait_for("message", timeout=120, check=check)
         except asyncio.TimeoutError:
             return await ctx.send("You took too long. Try again.")
 
         embed = discord.Embed(color=await ctx.embed_colour(), timestamp=datetime.now())
-        embed.set_author(name=f"{ctx.author.name} is Looking For Rocket League group!", icon_url=author.avatar_url)
+        embed.set_author(name="Looking For Group", icon_url=author.avatar_url)
         embed.set_footer(text="{0}#{1} ({2})".format(author.name, author.discriminator, author.id))
-        embed.add_field(name="GameMode:", value=gamemode.content, inline=False)
-        embed.add_field(name="Rank:", value=rank.content, inline=False)
-        embed.add_field(name="Server/s:", value=server.content, inline=False)
-        embed.add_field(name="Platform:", value=platform.content, inline=False)
-        embed.add_field(name="Looking for amount:", value=amount.content, inline=False)
-        await message.delete()
+        embed.add_field(name="GameMode:", value=gamemode, inline=True)
+        embed.add_field(name="Rank:", value=rank.content, inline=True)
+        embed.add_field(name="Server/s:", value=server.content, inline=True)
+        embed.add_field(name="Platform:", value=platform.content, inline=True)
+        embed.add_field(name="Looking for amount:", value=amount.content, inline=True)
+        await delete.message
 
         try:
             if await self.config.lfg_channel() is None:
@@ -90,7 +97,7 @@ class LookingFG(Cog):
                 channel = self.bot.get_channel(await self.config.lfg_channel())
                 await channel.send(embed=embed)
                 await author.send(
-                    "**Your LFG request has been made!**"
+                    "Your LFG request has been made!"
                 )
         except discord.Forbidden:
             await author.send("That didn't work for some reason")
