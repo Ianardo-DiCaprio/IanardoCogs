0fa5cbdf4aed5babaed68d33a9d7314c54a99c45
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-03-03 02:46:16 +0000

    Update sixmans.py

--- sixmans/sixmans.py
+++ sixmans/sixmans.py
@@ -358,13 +358,11 @@ class SixMans(commands.Cog):
 
         async with self.config.guild(ctx.guild).latest_game_number.get_lock():
             next_game_number = await self.config.guild(ctx.guild).latest_game_number() + 1
-            for player in self.game.blue:
-                await self.config.custom("GAMES", ctx.guild.id, next_game_number).blue.set(player.id)
-            for player in self.game.orange:
-                await self.config.custom("GAMES", ctx.guild.id, next_game_number).orange.set(list(player.id))
-                await self.config.guild(ctx.guild).latest_game_number.set(next_game_number)
-                test = await self.config.custom("GAMES", ctx.guild.id, next_game_number).orange()
-                await ctx.send(test)
+            await self.config.custom("GAMES", ctx.guild.id, next_game_number).blue.set([player.id for player in self.game.blue])
+            await self.config.custom("GAMES", ctx.guild.id, next_game_number).orange.set([player.id for player in self.game.orange])
+            await self.config.guild(ctx.guild).latest_game_number.set(next_game_number)
+            test = await self.config.custom("GAMES", ctx.guild.id, next_game_number).orange()
+            await ctx.send(test)
 
     async def create_game(self, ctx):
         team_size = await self.config.guild(ctx.guild).team_size()
