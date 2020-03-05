d267772b33829db4998770bc56cc530fb5be6eed
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-03-05 12:10:39 +0000

    Add files via upload

--- sixmans/sixmans.py
+++ sixmans/sixmans.py
@@ -129,11 +129,21 @@ class SixMans(commands.Cog):
     async def smclear(self, ctx):
         """Command to clear the queue"""
         if self.queue.empty() == False:
-            await ctx.send(self.queue.get())
+            self.queue.get()
         if self.queue.empty() == False:
-            await ctx.send(self.queue.get())
+            self.queue.get()
         if self.queue.empty() == False:
-            await ctx.send(self.queue.get())
+            self.queue.get()
+        if self.queue.empty() == False:
+            self.queue.get()
+        if self.queue.empty() == False:
+            self.queue.get()
+        if self.queue.empty() == False:
+            self.queue.get()
+        if self.queue.empty() == False:
+            self.queue.get()
+        if self.queue.empty() == False:
+            self.queue.get()
         cleared = (
             "**{}** cleared the queue.".format(ctx.author.display_name))
         embed = discord.Embed(description=cleared, color=0x00FFFF)
