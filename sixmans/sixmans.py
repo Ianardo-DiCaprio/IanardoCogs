a3e70ab5692d0238c2d87166463609d0f40c3223
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-03-06 01:03:21 +0000

    Add files via upload

--- sixmans/sixmans.py
+++ sixmans/sixmans.py
@@ -446,10 +446,8 @@ class SixMans(commands.Cog):
         channelnameorange = "Orange Team"
         createdorange = await ctx.guild.create_voice_channel(channelnameorange)
         self.game = Game(players)
-        for player in self.game.blue:
-            await ctx.send(player.id)
-        for player in self.game.orange:
-            await ctx.send(player.id)
+        await ctx.send(player.display_name for player in self.game.blue)
+        await ctx.send(player.display_name for player in self.game.orange)
 
     @commands.command(aliases=["smr"])
     async def smreport(self, ctx, code: int, winorloss):
