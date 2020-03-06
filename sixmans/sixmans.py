a8992aff45e6443fcd7f0c92bbe93007a6bf93cf
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-03-06 02:35:07 +0000

    Add files via upload

--- sixmans/sixmans.py
+++ sixmans/sixmans.py
@@ -71,7 +71,7 @@ class SixMans(commands.Cog):
 
         self.queue.put(player)
         if ctx.author.voice is None:
-            novc = ("{}:Please join a VC to join the queue.".format(ctx.author.mention))
+            novc = ("{}: Please join a VC to join the queue.".format(ctx.author.mention))
             embed = discord.Embed(description=novc, color=0x00FFFF)
             embed.set_author(name="VOID ESPORTS™ 6Mans", icon_url="https://cdn.discordapp.com/attachments/648743379252805663/684605565946953744/octopus-1.png")
             await ctx.send(embed=embed)
