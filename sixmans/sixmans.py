043a81ff418941dddacc4ead3210e117f0e4b80c
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-03-07 08:04:14 +0000

    Add files via upload

--- sixmans/sixmans.py
+++ sixmans/sixmans.py
@@ -488,11 +488,6 @@ class SixMans(commands.Cog):
             icon_url="https://cdn.discordapp.com/attachments/648743379252805663/684605565946953744/octopus-1.png",
         )
         await ctx.send(embed=embed)
-        await ctx.send(
-            "{}".format(
-                ", ".join([player.mention for player in self.game.players])
-            )
-        )
         await self.display_teams(ctx)
         await self.make_channel(ctx)
 
