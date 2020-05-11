47f79614890777e2121de39f5f42ebc3b23642b8
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-05-11 01:10:24 +0100

    Add files via upload

--- pdclockin/pdclockin.py
+++ pdclockin/pdclockin.py
@@ -7,11 +7,13 @@ from redbot.core import Config, checks, commands
 from redbot.core.bot import Red
 from redbot.core.utils.predicates import MessagePredicate, ReactionPredicate
 from redbot.core.utils.menus import start_adding_reactions
+from datetime import datetime
+from pytz import timezone
 
 Cog: Any = getattr(commands, "Cog", object)
 
 
-class LookingFG(Cog):
+class PDClockin(Cog):
     """
     Simple LFG commands
     """
@@ -21,8 +23,59 @@ class LookingFG(Cog):
 
     def __init__(self, bot: Red):
         self.bot = bot
-        self.config = Config.get_conf(self, 6991142013, force_registration=True)
+        self.config = Config.get_conf(self, 699114013, force_registration=True)
 
-        default_global = {"lfg_channel": None}
+        default_user = {"name": None, "message": None}
+        default_global = {"PDclock_channel": None}
 
         self.config.register_global(**default_global)
+        self.config.register_global(**default_user)
+
+    @commands.group(name="pdclock", autohelp=True)
+    async def _pdclock(self, ctx: commands.Context):
+        """
+        PD clock-in commands
+        """
+
+    @_pdclock.command()
+    @commands.guild_only()
+    async def name(self, ctx: commands.Context, name=None):
+        """set your IC name for PD clock-in's."""
+        if name:
+            await self.conf.user(ctx.author).name.set(name)
+            await ctx.send("Your PD name has been set.")
+        else:
+            await self.conf.user(ctx.author).name.set(name)
+            await ctx.send("Your PD name has been removed.")
+
+    @_pdclock.command()
+    @commands.guild_only()
+    async def channel(self, ctx, channel: discord.TextChannel = None):
+        """Set the channel clockins get sent to."""
+        if channel is None:
+            await self.config.PDclock_channel.set(channel)
+            await ctx.send("PD clock-in's has been disabled")
+        else:
+            await self.config.PDclock_channel.set(channel.id)
+            await ctx.send(
+                ("The PD clock-in's channel has been set to {channel.mention}").format(
+                    channel=channel
+                )
+            )
+
+    @commands.group()
+    async def pdclockin(self, ctx: commands.Context):
+        """
+        clock into PD
+        """
+        channel_id = await self.conf.guild(ctx.guild).PDclock_channel()
+        if not channel_id:
+            await ctx.send("The channel has not been set up to use this feature.")
+        else:
+            channel = guild.get_channel(channel_id)
+        name = await self.conf.user(ctx.author).name()
+        if not name:
+            name = ctx.author.display_name
+        tz = timezone('EST')
+        time = datetime.now(tz)
+        msg = await channel.send("{name} clocked into PD at {time}")
