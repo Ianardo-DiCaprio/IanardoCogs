f9e20c195480ecedb2f73f2209ccfa4f939eee22
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-05-11 02:47:50 +0100

    Update pdclockin.py

--- pdclockin/pdclockin.py
+++ pdclockin/pdclockin.py
@@ -69,7 +69,7 @@ class PDClockin(Cog):
         tz = timezone('EST5EDT')
         now = datetime.now(tz)
         time = now.strftime("%H:%M")
-        authormention = ctx.author.mention
+        authormention = ctx.author.display_name
         msg = await channel.send(f"**Discord name:** {authormention}\n**Clocked in:** {time}\n")
         await self.config.user(ctx.author).message.set(msg.content)
         await self.config.user(ctx.author).messageid.set(msg.id)
