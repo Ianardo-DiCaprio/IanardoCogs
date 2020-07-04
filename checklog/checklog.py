e41e7445c0bd30d0b08fec021d3f4e42430bd933
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2020-07-04 23:36:50 +0100

    Update checklog.py

--- checklog/checklog.py
+++ checklog/checklog.py
@@ -39,3 +39,6 @@ class CheckLog(commands.Cog):
             newermessage = str(newmessage).strip('[]')
             if int(newermessage) > 500000:
                 await message.channel.send(f"<@&689100532690059274>")
+        if message.channel.id == channel:
+            if "AP pistol" in message.content:
+                await message.channel.send(f"<@&689100532690059274>")
