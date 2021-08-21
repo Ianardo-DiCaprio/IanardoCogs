aa966a188c75f02e87c163904de7f2edaa6cbfb2
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2021-08-21 21:02:01 +0100

    Add files via upload

--- splitgate/splitgate.py
+++ splitgate/splitgate.py
@@ -139,6 +139,38 @@ class SPLITGATE(commands.Cog):
 
             if data["data"]["platformInfo"]["platformUserHandle"] != "N/A":
                 username = data["data"]["platformInfo"]["platformUserHandle"]
+            embed = discord.Embed(title=f"Splitgate Stats - {username} - **Page 3**", color=0x8C05D2)
+            if data["data"]["segments"][0]["stats"]["headshotAccuracy"]["displayValue"] != "N/A":
+                headshotAccuracy = data["data"]["segments"][0]["stats"]["headshotAccuracy"]["displayValue"]
+                embed.add_field(name="**Headshot Accuracy:**", value=headshotAccuracy, inline=True)
+            if data["data"]["segments"][0]["stats"]["shotsAccuracy"]["displayValue"] != "N/A":
+                shotsAccuracy = data["data"]["segments"][0]["stats"]["shotsAccuracy"]["displayValue"]
+                embed.add_field(name="**Accuracy:**", value=shotsAccuracy, inline=True)
+            if data["data"]["segments"][0]["stats"]["shotsFired"]["value"] != "N/A":
+                shotsFired = data["data"]["segments"][0]["stats"]["shotsFired"]["value"]
+                embed.add_field(name="**Shots Fired:**", value=shotsFired, inline=True)
+            if data["data"]["segments"][0]["stats"]["shotsLanded"]["vValue"] != "N/A":
+                shotsLanded = data["data"]["segments"][0]["stats"]["shotsLanded"]["value"]
+                embed.add_field(name="**Shots Landed:**", value=shotsLanded, inline=True)
+            if data["data"]["segments"][0]["stats"]["kd"]["value"] != "N/A":
+                kd = data["data"]["segments"][0]["stats"]["kd"]["value"]
+                embed.add_field(name="**K/D:**", value=kd, inline=True)
+            if data["data"]["segments"][0]["stats"]["kad"]["value"] != "N/A":
+                kad = data["data"]["segments"][0]["stats"]["kad"]["value"]
+                embed.add_field(name="**KA/D:**", value=kad, inline=True)
+            if data["data"]["segments"][0]["stats"]["killsPerMinute"]["value"] != "N/A":
+                killsPerMinute = data["data"]["segments"][0]["stats"]["killsPerMinute"]["value"]
+                embed.add_field(name="**Kills Per Minute:**", value=killsPerMinute, inline=True)
+            if data["data"]["segments"][0]["stats"]["killsPerMatch"]["value"] != "N/A":
+                killsPerMatch = data["data"]["segments"][0]["stats"]["killsPerMatch"]["value"]
+                embed.add_field(name="**Kills Per Match:**", value=killsPerMatch, inline=True)
+            if data["data"]["segments"][0]["stats"]["teabags"]["value"] != "N/A":
+                teabags = data["data"]["segments"][0]["stats"]["teabags"]["value"]
+                embed.add_field(name="**Teabags:**", value=teabags, inline=True)
+            embeds.append(embed)
+
+            if data["data"]["platformInfo"]["platformUserHandle"] != "N/A":
+                username = data["data"]["platformInfo"]["platformUserHandle"]
             embed = discord.Embed(title=f"Splitgate Stats - {username} - **Page 4**", color=0x8C05D2)
             if data["data"]["segments"][0]["stats"]["progressionXp"]["value"] != "N/A":
                 progressionXp = data["data"]["segments"][0]["stats"]["progressionXp"]["value"]
