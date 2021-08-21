6e769b58af3881100e29ebc4792414cfaed22be0
Ianardo DiCaprio <43935737+Ianardo-DiCaprio@users.noreply.github.com>
2021-08-21 15:12:31 +0100

    Add files via upload

--- splitgate/splitgate.py
+++ splitgate/splitgate.py
@@ -60,14 +60,90 @@ class SPLITGATE(commands.Cog):
             ) as request:
                 data = await request.json()
         try:
-            username = data["data"]["platformInfo"]["platformUserHandle"]
-            embed = discord.Embed(title="Splitgate Stats", color=0x8C05D2)
             if data["data"]["platformInfo"]["platformUserHandle"] != "N/A":
                 username = data["data"]["platformInfo"]["platformUserHandle"]
-                embed.add_field(name="**Username:**", value=username, inline=True)
+            embed = discord.Embed(title=f"Splitgate Stats - {username}", color=0x8C05D2)
             if data["data"]["segments"][0]["stats"]["kills"]["value"] != "N/A":
                 kills = data["data"]["segments"][0]["stats"]["kills"]["value"]
                 embed.add_field(name="**kills:**", value=kills, inline=True)
+            if data["data"]["segments"][0]["stats"]["assists"]["value"] != "N/A":
+                assists = data["data"]["segments"][0]["stats"]["assists"]["value"]
+                embed.add_field(name="**Assists:**", value=assists, inline=True)
+            if data["data"]["segments"][0]["stats"]["deaths"]["value"] != "N/A":
+                deaths = data["data"]["segments"][0]["stats"]["deaths"]["value"]
+                embed.add_field(name="**Deaths:**", value=deaths, inline=True)
+            if data["data"]["segments"][0]["stats"]["suicides"]["value"] != "N/A":
+                suicides = data["data"]["segments"][0]["stats"]["suicides"]["value"]
+                embed.add_field(name="**Suicides:**", value=suicides, inline=True)
+            if data["data"]["segments"][0]["stats"]["teabags"]["value"] != "N/A":
+                teabags = data["data"]["segments"][0]["stats"]["teabags"]["value"]
+                embed.add_field(name="**Teabags:**", value=teabags, inline=True)
+            if data["data"]["segments"][0]["stats"]["meleeKills"]["value"] != "N/A":
+                meleeKills = data["data"]["segments"][0]["stats"]["meleeKills"]["value"]
+                embed.add_field(name="**Melee Kills:**", value=meleeKills, inline=True)
+            if data["data"]["segments"][0]["stats"]["portalKills"]["value"] != "N/A":
+                portalKills = data["data"]["segments"][0]["stats"]["portalKills"]["value"]
+                embed.add_field(name="**Portal Kills:**", value=portalKills, inline=True)
+            if data["data"]["segments"][0]["stats"]["killsThruPortal"]["value"] != "N/A":
+                killsThruPortal = data["data"]["segments"][0]["stats"]["killsThruPortal"]["value"]
+                embed.add_field(name="**Kills Thru Portal:**", value=killsThruPortal, inline=True)
+            if data["data"]["segments"][0]["stats"]["damageDealt"]["value"] != "N/A":
+                damageDealt = data["data"]["segments"][0]["stats"]["damageDealt"]["value"]
+                embed.add_field(name="**Damage Dealt:**", value=damageDealt, inline=True)
+            if data["data"]["segments"][0]["stats"]["matchesPlayed"]["value"] != "N/A":
+                matchesPlayed = data["data"]["segments"][0]["stats"]["matchesPlayed"]["value"]
+                embed.add_field(name="**Matches Played:**", value=matchesPlayed, inline=True)
+            if data["data"]["segments"][0]["stats"]["wins"]["value"] != "N/A":
+                wins = data["data"]["segments"][0]["stats"]["wins"]["value"]
+                embed.add_field(name="**Wins:**", value=wins, inline=True)
+            if data["data"]["segments"][0]["stats"]["losses"]["value"] != "N/A":
+                losses = data["data"]["segments"][0]["stats"]["losses"]["value"]
+                embed.add_field(name="**Losses:**", value=losses, inline=True)
+            if data["data"]["segments"][0]["stats"]["wlPercentage"]["value"] != "N/A":
+                wlPercentage = data["data"]["segments"][0]["stats"]["wlPercentage"]["value"]
+                embed.add_field(name="**Win/Loss Percent:**", value=wlPercentage + "%", inline=True)
+            if data["data"]["segments"][0]["stats"]["timePlayed"]["displayValue"] != "N/A":
+                timePlayed = data["data"]["segments"][0]["stats"]["timePlayed"]["displayValue"]
+                embed.add_field(name="**Time Played:**", value=timePlayed, inline=True)
+            if data["data"]["segments"][0]["stats"]["headshotsLanded"]["value"] != "N/A":
+                headshotsLanded = data["data"]["segments"][0]["stats"]["headshotsLanded"]["value"]
+                embed.add_field(name="**Headshots Landed:**", value=headshotsLanded, inline=True)
+            if data["data"]["segments"][0]["stats"]["collaterals"]["value"] != "N/A":
+                collaterals = data["data"]["segments"][0]["stats"]["collaterals"]["value"]
+                embed.add_field(name="**Collaterals:**", value=collaterals, inline=True)
+            if data["data"]["segments"][0]["stats"]["headshotKills"]["value"] != "N/A":
+                headshotKills = data["data"]["segments"][0]["stats"]["headshotKills"]["value"]
+                embed.add_field(name="**Headshot Kills:**", value=headshotKills, inline=True)
+            if data["data"]["segments"][0]["stats"]["headshotAccuracy"]["value"] != "N/A":
+                headshotAccuracy = data["data"]["segments"][0]["stats"]["headshotAccuracy"]["value"]
+                embed.add_field(name="**Headshot Accuracy:**", value=headshotAccuracy + "%", inline=True)
+            if data["data"]["segments"][0]["stats"]["shotsFired"]["value"] != "N/A":
+                shotsFired = data["data"]["segments"][0]["stats"]["shotsFired"]["value"]
+                embed.add_field(name="**Shots Fired:**", value=shotsFired, inline=True)
+            if data["data"]["segments"][0]["stats"]["shotsLanded"]["value"] != "N/A":
+                shotsLanded = data["data"]["segments"][0]["stats"]["shotsLanded"]["value"]
+                embed.add_field(name="**Shots Landed:**", value=shotsLanded, inline=True)
+            if data["data"]["segments"][0]["stats"]["shotsAccuracy"]["value"] != "N/A":
+                shotsAccuracy = data["data"]["segments"][0]["stats"]["shotsAccuracy"]["value"]
+                embed.add_field(name="**Shots Accuracy:**", value=shotsAccuracy + "%", inline=True)
+            if data["data"]["segments"][0]["stats"]["kd"]["value"] != "N/A":
+                kd = data["data"]["segments"][0]["stats"]["kd"]["value"]
+                embed.add_field(name="**K/D:**", value=kd, inline=True)
+            if data["data"]["segments"][0]["stats"]["kad"]["value"] != "N/A":
+                kad = data["data"]["segments"][0]["stats"]["kad"]["value"]
+                embed.add_field(name="**KA/D:**", value=kad, inline=True)
+            if data["data"]["segments"][0]["stats"]["killsPerMinute"]["value"] != "N/A":
+                killsPerMinute = data["data"]["segments"][0]["stats"]["killsPerMinute"]["value"]
+                embed.add_field(name="**Kills Per Minute**", value=killsPerMinute, inline=True)
+            if data["data"]["segments"][0]["stats"]["killsPerMatch"]["value"] != "N/A":
+                killsPerMatch = data["data"]["segments"][0]["stats"]["killsPerMatch"]["value"]
+                embed.add_field(name="**Kills Per Match**", value=killsPerMatch, inline=True)
+            if data["data"]["segments"][0]["stats"]["medalDoubleKills"]["value"] != "N/A":
+                medalDoubleKills = data["data"]["segments"][0]["stats"]["medalDoubleKills"]["value"]
+                embed.add_field(name="**Double Kills**", value=medalDoubleKills, inline=True)
+            if data["data"]["segments"][0]["stats"]["medalTripleKills"]["value"] != "N/A":
+                medalTripleKills = data["data"]["segments"][0]["stats"]["medalTripleKills"]["value"]
+                embed.add_field(name="**Triple Kills**", value=medalTripleKills, inline=True)
             embeds.append(embed)
             await menu(
                 ctx, pages=embeds, controls=DEFAULT_CONTROLS, message=None, page=0, timeout=180)
